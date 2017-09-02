7bea846eaea0a4e1325290996be974ef262bf7df
badfilemagic <badfilemagic@users.noreply.github.com>
2017-09-02 16:45:28 -0500

    Update totalentropygather.d

--- totalentropygather.d
+++ totalentropygather.d
@@ -3,9 +3,9 @@
  * wdfreeman@ieee.org
  */
 
-/* this pulls a hex dump of the contents of arg0 up to 540 bytes,
+/* this pulls a hex dump of the contents of arg0 up to 580 bytes,
  * which was chosen after manual inspection of what gets fed in.
- * 540 bytes was the largest observed input.  Most of what will
+ * 580 bytes was the largest observed input.  Most of what will
  * seen is actually just 16 bytes
  */
 fbt::randomdev_hash_iterate:entry {
